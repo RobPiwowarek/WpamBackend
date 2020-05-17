a84456c5024f5d9c007c792c888d99075f86ad8a
Robert Piwowarek <robpiwowarek@gmail.com>
2020-05-17 21:07:54 +0200

    working login, register, webservice, schema

--- wpam/src/main/java/pl/rpw/wpam/UserController.java
+++ wpam/src/main/java/pl/rpw/wpam/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class UserController {
 
@@ -22,33 +24,31 @@ public class UserController {
     @CrossOrigin
     @PostMapping("/login")
     public boolean login(@RequestBody User user) {
-        User fetched = jdbcTemplate.queryForObject(
-                "SELECT * FROM USERS WHERE EMAIL = :email", new Object[]{user.getEmail()}, new UserRowMapper()
+        List<User> fetched = jdbcTemplate.query(
+                "SELECT * FROM USERS WHERE EMAIL = ?", new Object[]{user.getEmail()}, new UserRowMapper()
         );
 
-        if (fetched == null) {
+        if (fetched.size() != 1)
             return false;
+
+        if (fetched.get(0).getEmail().equals(user.getEmail()) && fetched.get(0).getPassword().equals(user.getPassword())) {
+            return true;
         } else {
-            if (fetched.getEmail().equals(user.getEmail()) && fetched.getPassword().equals(user.getPassword())) {
-                return true;
-            } else {
-                return false;
-            }
+            return false;
         }
     }
 
     @CrossOrigin
     @PutMapping("/register")
     public boolean register(@RequestBody User user) {
-        User fetched = jdbcTemplate.queryForObject(
-                "SELECT * FROM USERS WHERE EMAIL = :email", new Object[]{user.getEmail()}, new UserRowMapper()
+        List<User> fetched = jdbcTemplate.query(
+                "SELECT * FROM USERS WHERE EMAIL = ?", new Object[]{user.getEmail()}, new UserRowMapper()
         );
 
-        if (fetched == null) {
-            jdbcTemplate.update("INSERT INTO USERS VALUES (?, ?)", user.getEmail(), user.getPassword());
-            return true;
-        } else {
+        if (fetched.size() != 0)
             return false;
-        }
+
+        jdbcTemplate.update("INSERT INTO USERS VALUES (?, ?)", user.getEmail(), user.getPassword());
+        return true;
     }
 }
