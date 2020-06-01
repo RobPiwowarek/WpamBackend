a95eb3f4eb7e0421eceae0827f34c03832998f8a
Robert Piwowarek <robpiwowarek@gmail.com>
2020-06-01 11:52:37 +0200

    Stats saving

--- wpam/src/main/java/pl/rpw/wpam/UserController.java
+++ wpam/src/main/java/pl/rpw/wpam/UserController.java
@@ -1,7 +1,6 @@
 package pl.rpw.wpam;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,26 +15,9 @@ public class UserController {
     }
 
     @CrossOrigin
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        return "greeting";
-    }
-
-    @CrossOrigin
     @PutMapping("/login")
     public boolean login(@RequestBody User user) {
-        List<User> fetched = jdbcTemplate.query(
-                "SELECT * FROM USERS WHERE EMAIL = ?", new Object[]{user.getEmail()}, new UserRowMapper()
-        );
-
-        if (fetched.size() != 1)
-            return false;
-
-        if (fetched.get(0).getEmail().equals(user.getEmail()) && fetched.get(0).getPassword().equals(user.getPassword())) {
-            return true;
-        } else {
-            return false;
-        }
+        return authorize(user.getEmail(), user.getPassword());
     }
 
     @CrossOrigin
@@ -44,13 +26,6 @@ public class UserController {
         List<User> fetched = jdbcTemplate.query(
                 "SELECT * FROM USERS WHERE EMAIL = ?", new Object[]{user.getEmail()}, new UserRowMapper()
         );
-
-        System.out.println("fetched size " + fetched.size());
-
-        for (User a: fetched) {
-            System.out.println(a.getEmail());
-            System.out.println(a.getPassword());
-        }
 
         if (fetched.size() != 0)
             return false;
@@ -62,26 +37,21 @@ public class UserController {
     @CrossOrigin
     @PutMapping("/save")
     public boolean save(@RequestBody SaveRequest request) {
-        List<User> fetchedUsers = jdbcTemplate.query(
-                "SELECT * FROM USERS WHERE EMAIL = ?", new Object[]{request.getEmail()}, new UserRowMapper()
-        );
-
-        if (fetchedUsers.size() != 1)
-            return false;
-
-        if (fetchedUsers.get(0).getEmail().equals(request.getEmail()) && fetchedUsers.get(0).getPassword().equals(request.getPassword())) {
+        if (authorize(request.getEmail(), request.getPassword())) {
             List<Texture> fetched = jdbcTemplate.query(
                     "SELECT * FROM TEXTURES WHERE EMAIL = ?", new Object[]{request.getEmail()}, new TextureRowMapper()
             );
 
-            System.out.println("fetched size " + fetched.size());
+            jdbcTemplate.update("INSERT INTO TEXTURES VALUES (?, ?)", request.getEmail(), request.getTexture());
 
-            for (Texture a: fetched) {
-                System.out.println(a.getEmail());
-                System.out.println(a.getTexture());
+            List<Stats> fetchedStats = jdbcTemplate.query(
+                    "SELECT * FROM STATS WHERE EMAIL = ?", new Object[]{request.getEmail()}, new StatsRowMapper()
+            );
+
+            if (fetched.size() == 0) {
+                jdbcTemplate.update("INSERT INTO STATS VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", request.getEmail(), "Name...", 1, 1, 1, 1, 1, 0, 0, 0, 0, 10);
             }
 
-            jdbcTemplate.update("INSERT INTO TEXTURES VALUES (?, ?)", request.getEmail(), request.getTexture());
             return true;
         } else {
             return false;
@@ -91,28 +61,50 @@ public class UserController {
     @CrossOrigin
     @PutMapping("/load")
     public String load(@RequestBody User request) {
-        List<User> fetchedUsers = jdbcTemplate.query(
-                "SELECT * FROM USERS WHERE EMAIL = ?", new Object[]{request.getEmail()}, new UserRowMapper()
-        );
-
-        if (fetchedUsers.size() != 1)
-            return "FAILURE";
-
-        if (fetchedUsers.get(0).getEmail().equals(request.getEmail()) && fetchedUsers.get(0).getPassword().equals(request.getPassword())) {
+        if (authorize(request.getEmail(), request.getPassword())) {
             List<Texture> fetched = jdbcTemplate.query(
                     "SELECT * FROM TEXTURES WHERE EMAIL = ?", new Object[]{request.getEmail()}, new TextureRowMapper()
             );
-
-            System.out.println("fetched size " + fetched.size());
-
-            for (Texture a: fetched) {
-                System.out.println(a.getEmail());
-                System.out.println(a.getTexture());
-            }
 
             return fetched.get(0).getTexture();
         } else {
             return "UNAUTHORISED";
         }
+    }
+
+    @CrossOrigin
+    @PutMapping("/loadStats")
+    public Stats loadStats(@RequestBody User request) {
+        if (authorize(request.getEmail(), request.getPassword())) {
+            List<Stats> fetched = jdbcTemplate.query(
+                    "SELECT * FROM STATS WHERE EMAIL = ?", new Object[]{request.getEmail()}, new StatsRowMapper()
+            );
+
+            return fetched.get(0);
+        } else {
+            throw new IllegalStateException("UNAUTHORISED");
+        }
+    }
+
+    @CrossOrigin
+    @PutMapping("/saveStats")
+    public boolean saveStats(@RequestBody StatsSaveRequest request) {
+        if (authorize(request.getEmail(), request.getPassword())) {
+            jdbcTemplate.update("INSERT INTO STATS VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", request.getEmail(), request.getName(), request.getLevel(), request.getDamage(), request.getHealth(), request.getEnergy(), request.getHealing(), request.getArmor(), request.getCrit(), request.getStun(), request.getEvade(), request.getPoints());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean authorize(String email, String password) {
+        List<User> fetchedUsers = jdbcTemplate.query(
+                "SELECT * FROM USERS WHERE EMAIL = ?", new Object[]{email}, new UserRowMapper()
+        );
+
+        if (fetchedUsers.size() != 1)
+            return false;
+
+        return fetchedUsers.get(0).getEmail().equals(email) && fetchedUsers.get(0).getPassword().equals(password);
     }
 }
